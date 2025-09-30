b80e6853006cc297dd654733dc5512c6841e1e82
kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-09-30 20:16:35 +0200

    moving RayCastLogic to the separate script

--- Projectile/PenetrationRayCast.gd
+++ Projectile/PenetrationRayCast.gd
@@ -1,4 +1,7 @@
-extends Node
+extends RayCast3D
+
+@export var OFFSET_DISTANCE = 0.01
+@export var RAY_LENGTH = 500.0
 
 @onready var ray_cast: RayCast3D = $RayCast3D
 
@@ -6,5 +9,15 @@ extends Node
 func measure_thickness(collision_angle: float) -> void:
 	pass
 
-func find_entry_point(direction: Vector3) -> void:
-	pass
+func _find_entry_point(origin: Vector3, direction: Vector3) -> void:
+	enabled = true
+	target_position = direction * RAY_LENGTH 
+	force_raycast_update()
+	
+	if not is_colliding():
+		enabled = false
+		push_warning("Ray did not hit the mesh.")
+		return
+	
+	var entry_point: Vector3 = get_collision_point()
+	var entry_normal: Vector3 = get_collision_normal()
