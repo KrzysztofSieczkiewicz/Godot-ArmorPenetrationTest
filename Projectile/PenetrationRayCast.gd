b60378a33b6f66cb6334f688debc08a930d4357c
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2025-09-30 21:52:58 +0200

    collider thickness checked by reverted raycast

--- Projectile/PenetrationRayCast.gd
+++ Projectile/PenetrationRayCast.gd
@@ -3,21 +3,40 @@ extends RayCast3D
 @export var OFFSET_DISTANCE = 0.01
 @export var RAY_LENGTH = 500.0
 
-@onready var ray_cast: RayCast3D = $RayCast3D
 
+func measure_thickness(origin: Vector3, direction: Vector3) -> void:
+	_find_entry_point(origin, direction)
 
-func measure_thickness(collision_angle: float) -> void:
-	pass
 
 func _find_entry_point(origin: Vector3, direction: Vector3) -> void:
 	enabled = true
 	target_position = direction * RAY_LENGTH 
 	force_raycast_update()
 	
-	if not is_colliding():
-		enabled = false
-		push_warning("Ray did not hit the mesh.")
-		return
-	
 	var entry_point: Vector3 = get_collision_point()
-	var entry_normal: Vector3 = get_collision_normal()
+	var contact_surface_normal: Vector3 = get_collision_normal()
+	
+	var OFFSET: float = 50.0;
+	var collision_vector = direction
+	var new_position = entry_point + direction * OFFSET
+	var new_direction = collision_vector.normalized() * (-1) * OFFSET
+	var new_target_position = new_position + new_direction * RAY_LENGTH
+	
+	var contact_angle = _get_impact_angle(direction, contact_surface_normal)
+	
+	global_transform.origin = new_position
+	target_position = to_local(new_target_position)
+	force_raycast_update()
+	
+	var exit_point: Vector3 = get_collision_point()
+	
+	var thickness = (exit_point - entry_point).length()
+	push_error("Thickness: %s" % thickness)
+
+
+func _get_impact_angle(projectile_direction: Vector3, surface_normal: Vector3) -> float:
+	var cos_angle = abs(projectile_direction.dot(surface_normal))
+	var angle_between_velocity_and_normal = acos(cos_angle)
+	var contact_angle = rad_to_deg(angle_between_velocity_and_normal)
+	return (90.0 - contact_angle)
+	
--- Projectile/Projectile_AP.gd
+++ Projectile/Projectile_AP.gd
@@ -7,6 +7,7 @@ extends CharacterBody3D
 var initial_direction: Vector3 = Vector3.ZERO 
 
 @onready var thickness_raycast: RayCast3D = $RayCast3D
+@onready var thickness_probe: RayCast3D = $PenetrationRayCast3D
 
 func _ready() -> void:
 	if initial_direction != Vector3.ZERO:
@@ -37,57 +38,9 @@ func _physics_process(delta: float) -> void:
 
 
 func _handle_collision(collision_info: KinematicCollision3D) -> void:
-	var body_hit = collision_info.get_collider()
 	
+	var impact_point: Vector3 = collision_info.get_position()
 	var impact_velocity: Vector3 = velocity.normalized()
-	var surface_normal: Vector3 = collision_info.get_normal()
 	
-	var cos_angle = abs(impact_velocity.dot(surface_normal))
-	var angle_between_velocity_and_normal = acos(cos_angle)
-	var angle_deg = rad_to_deg(angle_between_velocity_and_normal)
-	
-	var angle_of_impact = 90.0 - angle_deg 
-	
-	_trigger_raycast_collision_check()
-	#push_warning("Collision detected! Collision angle %s" % angle_of_impact)
+	thickness_probe.measure_thickness(impact_point, impact_velocity);
 	queue_free()
-
-func _trigger_raycast_collision_check() -> float:
-	var OFFSET_DISTANCE = 0.01
-	var RAY_LENGTH = 500.0
-
-	thickness_raycast.enabled = true
-	thickness_raycast.target_position = initial_direction.normalized() * RAY_LENGTH 
-	thickness_raycast.force_raycast_update()
-
-	if not thickness_raycast.is_colliding():
-		thickness_raycast.enabled = false
-		push_warning("Ray did not hit the mesh.")
-		return 0.0
-	
-	var entry_point: Vector3 = thickness_raycast.get_collision_point()
-	var entry_normal: Vector3 = thickness_raycast.get_collision_normal()
-	
-	var inward_direction: Vector3 = -entry_normal.normalized()
-	var ray_origin: Vector3 = entry_point + (inward_direction * OFFSET_DISTANCE)
-	
-	thickness_raycast.global_transform.origin = ray_origin
-	var global_end_point = ray_origin + inward_direction * RAY_LENGTH
-	thickness_raycast.target_position = thickness_raycast.to_local(global_end_point)
-	thickness_raycast.force_raycast_update()
-	
-	if not thickness_raycast.is_colliding():
-		thickness_raycast.enabled = false
-		push_warning("Ray entered mesh but did not find an exit point within bounds.")
-		return 0.0
-	
-	var exit_point: Vector3 = thickness_raycast.get_collision_point()
-	var thickness: float = entry_point.distance_to(exit_point)
-	
-	thickness_raycast.enabled = false
-	
-	print("Mesh Entry Point: ", entry_point)
-	print("Mesh Exit Point:  ", exit_point)
-	print("Calculated Thickness: ", thickness)
-	
-	return thickness
