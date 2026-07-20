b389be644dfecfe65966ad7fd772f1fbe68e49b6
kristoff <krzysztof.sieczkiewicz@gmail.com>
2026-07-20 19:00:11 +0200

    normalization wip

--- Projectile/AP/ap_shell.gd
+++ Projectile/AP/ap_shell.gd
@@ -33,12 +33,18 @@ create external manager class that will determine both armor and shell behaviour
 
 
 """
-NOW:
+PREV:
 	DONE - add another variable that determines position of the tip in relation to the center (or just tip position if better)
 	DONE - allow for rotating and moving the shell in relation to the tip point
 	DONE - on penetration - move the shell so the tip is on exit_point
-	- retrigger the probing for penetration(?) - shapecast is problematic here as it can collide with the same armor piece, but any exclusions will make it ignore the collider if it hits it again (e.g. internal corner pen)
+	DONE - retrigger the probing for penetration(?) - shapecast is problematic here as it can collide with the same armor piece, but any exclusions will make it ignore the collider if it hits it again (e.g. internal corner pen)
 		it seems that the best way forward is moving towards a single raycast (change to bundle later) to determine penetration behavior
+"""
+"""
+NOW:
+	- introduce early exit rotation
+	- figure out how to quickly calculate the penetration direction (should )
+	- rework collision to work as continuous check (sub-frame sim?) and work with entire series of colliders
 """
 """
 NEXT:
@@ -48,6 +54,7 @@ NEXT:
 		- check how to limit missing detection on hitting thin plate from the side
 	- when collision by shapecast is detected and the projectile goes into the penetration/non-penetration mode - expand the functions to handle the situation where the projectile will hit multiple surfaces at the nearly same time
 		and different events might occur (e.g. tight space between two angled plates - (double ricochet) or (ricochet and penetration for different surfaces). This might require handling certain redirections on hit / breaking collision group into separate events / special energy loss event
+	- cap the maximum armor gained from angling to 3-3.5 the plate thickness (due to assymetric breakout) or model it properly via turning the projectile during penetration
 """
 
 # TODO: move to general config later
@@ -80,12 +87,12 @@ const MASK_DYNAMIC = 2
 const MASK_PROJECTILE = 3
 
 class ResolutionPacket:
-	var impact_normal: Vector3
-	var impact_point: Vector3
-	var projectile_velocity: Vector3
-	var target_velocity: Vector3
-	var target_collider: Object
-	var relative_velocity: Vector3
+	var impact_normal: Vector3				# Surface normal on projectile impact
+	var impact_point: Vector3				# Global position of projectile impact
+	var projectile_velocity: Vector3		# Velocity of projectile on impact
+	var target_velocity: Vector3			# Velocity of object the projectile collided with
+	var target_collider: Object				# Collider
+	var relative_velocity: Vector3			# Final velocity (target+projectile)
 
 func launch() -> void:
 	current_velocity = -global_transform.basis.z * muzzle_velocity
@@ -120,7 +127,7 @@ func _run_ghost_mode(start: Vector3, motion: Vector3) -> Dictionary:								# TO
 	print("\n=================== [PHASE 1: GHOST MODE] ===================")
 	print("Shell Position : ", start)
 	print("Travel Vector  : ", motion, " (Distance this frame: ", motion.length(), "m)")
-	print("Checking Mask  : ", MASK_STATIC | MASK_DYNAMIC | MASK_PROJECTILE, " (Looking for Layers 1, 2, 3)")
+	#print("Checking Mask  : ", MASK_STATIC | MASK_DYNAMIC | MASK_PROJECTILE, " (Looking for Layers 1, 2, 3)")
 	
 	var query = PhysicsShapeQueryParameters3D.new()
 	query.shape = sweep_shape
@@ -203,16 +210,18 @@ func _process_balistic_resolver(packet: ResolutionPacket, remaining_delta: float
 	var td_ratio = armor_thickness / (2 * shell_radius)
 	var is_overmatch = td_ratio < 0.5 and impact_angle > deg_to_rad(45.0)
 	
-	print("Thickness / shell diameter ratio: ", td_ratio)
-	print("Overmatch: ", is_overmatch)
-	print("Is ricocheting: ", impact_angle > ricochet_angle_threshold_rad)
+	#print("Thickness / shell diameter ratio: ", td_ratio)
+	#print("Overmatch: ", is_overmatch)
+	#print("Is ricocheting: ", impact_angle > ricochet_angle_threshold_rad)
 	
-	if is_overmatch:
-		_handle_overmatch(packet, impact_angle, armor_thickness, td_ratio)
-	elif impact_angle > ricochet_angle_threshold_rad:
-		_handle_ricochet(packet, impact_angle)
-	else:
-		_handle_penetration(packet, impact_angle)
+	_handle_penetration(packet, impact_angle)
+	
+	#if is_overmatch:
+	#	_handle_overmatch(packet, impact_angle, armor_thickness, td_ratio)
+	#elif impact_angle > ricochet_angle_threshold_rad:
+	#	_handle_ricochet(packet, impact_angle)
+	#else:
+	#	_handle_penetration(packet, impact_angle)
 
 
 # TODO: review this function - graze factor logic seems to be off and required clamping as a quick fix
@@ -264,27 +273,36 @@ func _handle_overmatch(packet: ResolutionPacket, impact_angle: float, armor_thic
 
 func _handle_penetration(packet: ResolutionPacket, impact_angle: float):
 	var effective_angle = max(0.0, impact_angle - normalization_factor) # normalization
+	var effective_angle_rad = deg_to_rad(effective_angle)
+	
+	var test_rotation = packet.projectile_velocity.slerp(Vector3.UP, effective_angle_rad)								# TODO: normalization should occur only on first collision in group
+	print("Default impact direction: ", packet.projectile_velocity)
+	print("Post-normalization impact direction", test_rotation)
+	
 	var impact_vector_norm: Vector3 = packet.projectile_velocity.normalized()
 	var armor_probing_result = BallisticProber.probe_thickness(space_state, packet.impact_point, impact_vector_norm)
 	
-	push_warning("Penetration")
+	# TODO: remove later
+	var result
+	_transform_around_tip(armor_probing_result.entry_point, packet.projectile_velocity)
+	# End TODO
+	
 	print("\nPenetration:")
-	print("entry point: ", armor_probing_result.entry_point)
-	print("exit point: ", armor_probing_result.exit_point)
-	print("thickness: ", armor_probing_result.thickness)
-	
-	print("penetration direction: ", (armor_probing_result.exit_point - armor_probing_result.entry_point).normalized() )
-	print("initial pen direction: ", packet.projectile_velocity.normalized() )
-	
-	print("old global position: ", global_position)
+	print("	entry point: ", armor_probing_result.entry_point)
+	print("	exit point: ", armor_probing_result.exit_point)
+	print("	thickness: ", armor_probing_result.thickness)
+
 	_transform_around_tip(armor_probing_result.exit_point, packet.projectile_velocity)
-	print("new global position: ", global_position)
-	#queue_free()
-	current_velocity = Vector3.ZERO
+	
+	queue_free()
+	#current_velocity = Vector3.ZERO
 
+## Returns projectile velocity vector after the initial impact normalization
+func _normalize_projectile(impact_vector: Vector3, normalization_factor: float) -> void:
+	pass
 
-## Rotate the shell around tip point
-## Note: shell must be oriented along movement vector
+## Rotates the shell around tip point. 
+## TODO: might require double checking for edge cases
 func _transform_around_tip(position: Vector3, direction: Vector3) -> void:
 	var target_dir = direction.normalized()
 	var new_origin_position = position - (tip_to_center_distance * target_dir)
